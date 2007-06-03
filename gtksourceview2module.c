8e8b16891be651e58f68ad225eb12aeadf226d2a
Paolo Borelli <pborelli@src.gnome.org>
2007-06-03 14:46:28 +0000

    release 1.90.0
    svn path=/trunk/; revision=9

--- gtksourceview2module.c
+++ gtksourceview2module.c
@@ -26,29 +26,28 @@ void pygtksourceview2_register_classes (PyObject *d);
 void pygtksourceview2_add_constants(PyObject *module, const gchar *strip_prefix);
 
 extern PyMethodDef pygtksourceview2_functions[];
- 
+
 DL_EXPORT (void)
 initgtksourceview2 (void)
 {
     PyObject *m, *d;
-    
+
     m = Py_InitModule ("gtksourceview2", pygtksourceview2_functions);
     d = PyModule_GetDict (m);
-    
+
     init_pygobject ();
     init_pygtk ();
 
     pygtksourceview2_register_classes (d);
-    
+
     pygtksourceview2_add_constants(m, "GTK_");
-    
+
     PyModule_AddObject(m, "pygtksourceview2_version",
                        Py_BuildValue("iii",
-                                     PYGTKSOURCEVIEW2_MAJOR_VERSION,
-                                     PYGTKSOURCEVIEW2_MINOR_VERSION,
-                                     PYGTKSOURCEVIEW2_MICRO_VERSION));
-    
+                                     PYGTKSOURCEVIEW_MAJOR_VERSION,
+                                     PYGTKSOURCEVIEW_MINOR_VERSION,
+                                     PYGTKSOURCEVIEW_MICRO_VERSION));
+
     if (PyErr_Occurred ())
         Py_FatalError ("can't initialise module gtksourceview2");
-    
 }
